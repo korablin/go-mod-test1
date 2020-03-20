3b0bed56bf23d1ceab77d5671883d50c714c4739
Scott Yeskie <scott@yeskie.com>
2020-03-20 15:42:41 -0500

    Initial code commit with go modules framework and sample package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/s42ky/go-mod-test1
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/s42ky/go-mod-test1/foo"
+
+func main() {
+	println("Running go-mod-test1")
+	foo.Hello()
+}
