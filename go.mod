53da4865265fac2de930855b51a642e2c8f98cb9
nkorabli <nkorabli@cisco.com>
2021-11-16 19:59:45 +0300

    Add dep check

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/s42ky/go-mod-test1
 
-go 1.14
+go 1.17
--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/s42ky/go-mod-test1/foo"
+import(
+    "runtime"
+    "fmt"
+    "runtime/debug"
+    "github.com/s42ky/go-mod-test1/foo"
+)
 
 func main() {
 	println("Running go-mod-test1")
+    println(runtime.Version())
+
+    buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+        fmt.Printf("%v", ok)
+		fmt.Println("Can't read BuildInfo")
+	}
+	fmt.Println("Dependencies:")
+	for _, dep := range buildInfo.Deps {
+		fmt.Printf("  %s %s\n", dep.Path, dep.Version)
+	}
+
 	foo.Hello()
 }
