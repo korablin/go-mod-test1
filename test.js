a723f482b2abb17b3c4b4e22017f5cc7a24a0a68
Nikolai Korablin <korablinn@gmail.com>
2022-11-23 14:42:44 +0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+var params = document.body.getElementsById('myScript');
+query = params[0].classList;
+var param_a = query[0];
+var param_b = query[1];
+var param_c = query[2];
+console.log(param_a, param_b, param_c);
